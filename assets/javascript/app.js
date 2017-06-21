d35609a6500d5ba4afcaa0ffd31f70c030353ebc
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-06-20 22:55:26 -0700

    Working out skeleton. Planning out jQuery implementation for question pages.

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+//question and answer variables
+var q1 = ['question 1'. 'answer 1'];
+var q2 = ['question 2'. 'answer 2'];
+var q3 = ['question 3'. 'answer 3'];
+var q4 = ['question 4'. 'answer 4'];
+var q5 = ['question 5'. 'answer 5'];
+var q6 = ['question 6'. 'answer 6'];
+var q7 = ['question 7'. 'answer 7'];
+var q8 = ['question 8'. 'answer 8'];
+var q9 = ['question 9'. 'answer 9'];
+var q10 = ['question 10'. 'answer 10'];
+
+

dedec9899eb45bd346f07d212448b90d079948be
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-06-21 18:41:16 -0700

    Making question generation functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,73 +1,76 @@
 //question and answer variables
-var arrQandA;
+var questionAnswerBank;
 var arrWrongAnswers;
 var arrRandomizedChoices;
 
 var correctCount;
 var incorrectCount;
 
-var wrongAnswer1;
-var wrongAnswer2;
-var wrongAnswer3;
+var answerChoice
 
 var falseIndex = Math.floor(Math.random() * 3); //change 3 if arrWrongAnswers length changes
 
+var buttonDiv = '<div class="btn-group" role="group" aria-label="..."></div>'
 
+
+//function declarations
 function startUp() {
 	//list of questions;
-	arrQandA = [
-		['question 1', 'answer 1'],
-		['question 2', 'answer 2'],
-		['question 3', 'answer 3'],
-		['question 4', 'answer 4'],
-		['question 5', 'answer 5'],
-		['question 6', 'answer 6'],
-		['question 7', 'answer 7'],
-		['question 8', 'answer 8'],
-		['question 9', 'answer 9'],
-		['question 10', 'answer 10'],
-	]
+	questionAnswerBank = {
+		'What is 1?' : 'a1',
+		'What is 2?' : 'a2',
+		'What is 3?' : 'a3',
+		'What is 4?' : 'a4',
+		'What is 5?' : 'a5',
+		'What is 6?' : 'a6',
+		'What is 7?' : 'a7',
+		'What is 8?' : 'a8',
+		'What is 9?' : 'a9',
+		'What is 10?' : 'a10',
+	}
+	
+	arrWrongAnswers = {
+		false1 : ['false 1.1', 'false 1.2', 'false 1.3'],
+		false2 : ['false 2.1', 'false 2.2', 'false 2.3'],
+		false3 : ['false 3.1', 'false 3.2', 'false 3.3'],
+		false4 : ['false 4.1', 'false 4.2', 'false 4.3'],
+		false5 : ['false 5.1', 'false 5.2', 'false 5.3'],
+		false6 : ['false 6.1', 'false 6.2', 'false 6.3'],
+		false7 : ['false 7.1', 'false 7.2', 'false 7.3'],
+		false8 : ['false 8.1', 'false 8.2', 'false 8.3'],
+		false9 : ['false 9.1', 'false 9.2', 'false 9.3'],
+		false10 : ['false 10.1', 'false 10.2', 'false 10.3'],
+	}
 
-	arrWrongAnswers = [
-		['false 1.1', 'false 1.2', 'false 1.3'], //for question question 1
-		['false 2.1', 'false 2.2', 'false 2.3'], //for question question 2
-		['false 3.1', 'false 3.2', 'false 3.3'], //for question question 3
-		['false 4.1', 'false 4.2', 'false 4.3'], //for question question 4
-		['false 5.1', 'false 5.2', 'false 5.3'], //for question question 5
-		['false 6.1', 'false 6.2', 'false 6.3'], //for question question 6
-		['false 7.1', 'false 7.2', 'false 7.3'], //for question question 7
-		['false 8.1', 'false 8.2', 'false 8.3'], //for question question 8
-		['false 9.1', 'false 9.2', 'false 9.3'], //for question question 9
-		['false 10.1', 'false 10.2', 'false 10.3'], //for question question 10
-
-	]
-
-	wrongAnswer1 = '<button type="button" class="btn btn-default" id="answer1">'+ arrWrongAnswers[falseIndex]+'</button>';
-	wrongAnswer2 = '<button type="button" class="btn btn-default" id="answer1">'+ arrWrongAnswers[falseIndex]+'</button>';
-	wrongAnswer3 = '<button type="button" class="btn btn-default" id="answer1">'+ arrWrongAnswers[falseIndex]+'</button>';
-
+	answerChoice = '<div class="col-md-12 text-center display-text" id="start">' + 'tester' + '</div>';
 
 	correctCount = 0;
 	incorrectCount = 0;
+
 }
 
-function qGenerator() {
+function questionGenerator() {
+	$('div#start').html(answerChoice); //puts in the answerchoice here
+	
+
+	//might rework below so buttons are vertically stacked
+	for (var i = 0; i < 3; i++) { //iterates through falses and creates div for buttons
+		$(answerChoice).appendTo('div#question-options');
+		console.log('testing this working')
+	}
 	
 }
 
-$('button#start').click(function() {
-	$('div button#start').html('<button type="button" class="btn btn-default" id="answer1">'+ +'</button>');
-	$('div button#start').html('<button type="button" class="btn btn-default" id="answer2">'+ +'</button>');
-	$('div button#start').html('<button type="button" class="btn btn-default" id="answer3">'+ +'</button>');
-	$('div button#start').html('<button type="button" class="btn btn-default" id="answer4">'+ +'</button>');
-	});
+$('#start h3').click(function() {
+	questionGenerator();
+});
 
-
-//console.log('arrQandA-blank', arrQandA);
-//console.log('correctCount-blank', correctCount);
+//FUNCTION CALLS
 startUp();
-//console.log('arrQandA-initial-set', arrQandA);
-//console.log('correctCount-blank', correctCount);
+//questionGenerator();
+
+//console.log('keys', Object.keys(questionAnswerBank));
+//console.log('key1', Object.keys(questionAnswerBank)[6]);
 
 
 

c9cb85462e3fc3b00625c06dde260df0296611da
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-06-21 22:26:46 -0700

    Getting the program to work mostly. Need to edit timing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,26 +6,33 @@ var arrRandomizedChoices;
 var correctCount;
 var incorrectCount;
 
-var arrFalseChoice;
+var possibleChoices;
 
-var falseIndex = Math.floor(Math.random() * 3); //change 3 if arrWrongAnswers length changes
+var choice2;
+var choice3;
+var choice4;
 
-var buttonDiv = '<div class="btn-group" role="group" aria-label="..."></div>'
+var userChoice;
+
+//var falseIndex = Math.floor(Math.random() * 3); //change 3 if arrWrongAnswers length changes
+
+//var buttonDiv = '<div class="btn-group" role="group" aria-label="..."></div>'
 
 
 //function declarations
 function startUp() {
 	//list of questions;
 	questionAnswerBank = {
-		'Which of the following are an alien race bent on enslaving humankind?' : 
+		'Which of the following is an alien race bent on enslaving humankind?' : 
 			['Hollows', 'Goa\'uld', 'Death Eaters', 'Androids'],
 		'What is the name of the shield used to block the Stargate?' : 
 			['Iris', 'Anubis', 'Osiris', 'Block'],
 		'How many members are typically in a recon team to other worlds?' : 
 			['3', '4', '5', '6'],
 		'What state is the Stargate located in?' : 
-			['a4',],
-		'What is 5?' : 'a5',
+			['California', 'New Mexico', 'Colorado', 'Arizona'],
+		'Who is the doctor on the team?' : 
+			['Dr. Carter', 'Dr. Jackson', 'Dr. O\'Neill', 'Dr. Osborne'],
 		// 'What is 6?' : 'a6',
 		// 'What is 7?' : 'a7',
 		// 'What is 8?' : 'a8',
@@ -33,29 +40,40 @@ function startUp() {
 		// 'What is 10?' : 'a10',
 	}
 
-	falseChoice = {
-		f1 : '<div class="col-md-12 text-center display-text" id="start">' + 'tester' + '</div>',
-		f2 : '<div class="col-md-12 text-center display-text" id="start">' + 'tester2' + '</div>',
-		f3 : '<div class="col-md-12 text-center display-text" id="start">' + 'tester3' + '</div>',
-	}
-
 	correctCount = 0;
 	incorrectCount = 0;
-
 }
 
 function questionGenerator() {
-	//for (var i = 0; i < Object.keys(questionAnswerBank).length; i++) {
-		$('h2').html(Object.keys(questionAnswerBank)[0]);//change to i
-		//console.log('count 1');
+	var i = 0;
 
-		$('div#start').html(falseChoice); //puts in the arrFalseChoice here
-		//might rework below so buttons are vertically stacked
-		for (var j = 0; j < 3; j++) { //iterates through falses and creates div for buttons
-			$(falseChoice.f1).appendTo('div#question-options');
-			//console.log('testing this working')
-		}		
-	//}
+	while (i < 5) {
+		choice2 = Object.values(questionAnswerBank)[i][1];
+		choice3 = Object.values(questionAnswerBank)[i][2];
+		choice4 = Object.values(questionAnswerBank)[i][3];
+
+		possibleChoices = {
+			possibility2 : '<h3>' + choice2 + '</h3>',
+			possibility3 : '<h3>' + choice3 + '</h3>',
+			possibility4 : '<h3>' + choice4 + '</h3>',
+		}
+
+		$('h2').html(Object.keys(questionAnswerBank)[i]);
+		$('div#start').html('<h3>' + Object.values(questionAnswerBank)[i][0] + '</h3>');
+		$(possibleChoices.possibility2).appendTo('div#start');
+		$(possibleChoices.possibility3).appendTo('div#start');
+		$(possibleChoices.possibility4).appendTo('div#start');
+
+		//if (i = 0 && userChoice === "Goal") {
+
+		// }
+
+
+
+		console.log("counter");
+
+		i++;
+	}	
 }
 
 $('#start h3').click(function() {

ad69be8866edafbc3d78a295aa2eb343bba90614
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-06-21 21:04:31 -0700

    Updating questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ var arrRandomizedChoices;
 var correctCount;
 var incorrectCount;
 
-var answerChoice
+var arrFalseChoice;
 
 var falseIndex = Math.floor(Math.random() * 3); //change 3 if arrWrongAnswers length changes
 
@@ -17,32 +17,27 @@ var buttonDiv = '<div class="btn-group" role="group" aria-label="..."></div>'
 function startUp() {
 	//list of questions;
 	questionAnswerBank = {
-		'What is 1?' : 'a1',
-		'What is 2?' : 'a2',
-		'What is 3?' : 'a3',
-		'What is 4?' : 'a4',
+		'Which of the following are an alien race bent on enslaving humankind?' : 
+			['Hollows', 'Goa\'uld', 'Death Eaters', 'Androids'],
+		'What is the name of the shield used to block the Stargate?' : 
+			['Iris', 'Anubis', 'Osiris', 'Block'],
+		'How many members are typically in a recon team to other worlds?' : 
+			['3', '4', '5', '6'],
+		'What state is the Stargate located in?' : 
+			['a4',],
 		'What is 5?' : 'a5',
-		'What is 6?' : 'a6',
-		'What is 7?' : 'a7',
-		'What is 8?' : 'a8',
-		'What is 9?' : 'a9',
-		'What is 10?' : 'a10',
-	}
-	
-	arrWrongAnswers = {
-		false1 : ['false 1.1', 'false 1.2', 'false 1.3'],
-		false2 : ['false 2.1', 'false 2.2', 'false 2.3'],
-		false3 : ['false 3.1', 'false 3.2', 'false 3.3'],
-		false4 : ['false 4.1', 'false 4.2', 'false 4.3'],
-		false5 : ['false 5.1', 'false 5.2', 'false 5.3'],
-		false6 : ['false 6.1', 'false 6.2', 'false 6.3'],
-		false7 : ['false 7.1', 'false 7.2', 'false 7.3'],
-		false8 : ['false 8.1', 'false 8.2', 'false 8.3'],
-		false9 : ['false 9.1', 'false 9.2', 'false 9.3'],
-		false10 : ['false 10.1', 'false 10.2', 'false 10.3'],
+		// 'What is 6?' : 'a6',
+		// 'What is 7?' : 'a7',
+		// 'What is 8?' : 'a8',
+		// 'What is 9?' : 'a9',
+		// 'What is 10?' : 'a10',
 	}
 
-	answerChoice = '<div class="col-md-12 text-center display-text" id="start">' + 'tester' + '</div>';
+	falseChoice = {
+		f1 : '<div class="col-md-12 text-center display-text" id="start">' + 'tester' + '</div>',
+		f2 : '<div class="col-md-12 text-center display-text" id="start">' + 'tester2' + '</div>',
+		f3 : '<div class="col-md-12 text-center display-text" id="start">' + 'tester3' + '</div>',
+	}
 
 	correctCount = 0;
 	incorrectCount = 0;
@@ -50,15 +45,17 @@ function startUp() {
 }
 
 function questionGenerator() {
-	$('div#start').html(answerChoice); //puts in the answerchoice here
-	
+	//for (var i = 0; i < Object.keys(questionAnswerBank).length; i++) {
+		$('h2').html(Object.keys(questionAnswerBank)[0]);//change to i
+		//console.log('count 1');
 
-	//might rework below so buttons are vertically stacked
-	for (var i = 0; i < 3; i++) { //iterates through falses and creates div for buttons
-		$(answerChoice).appendTo('div#question-options');
-		console.log('testing this working')
-	}
-	
+		$('div#start').html(falseChoice); //puts in the arrFalseChoice here
+		//might rework below so buttons are vertically stacked
+		for (var j = 0; j < 3; j++) { //iterates through falses and creates div for buttons
+			$(falseChoice.f1).appendTo('div#question-options');
+			//console.log('testing this working')
+		}		
+	//}
 }
 
 $('#start h3').click(function() {
@@ -67,7 +64,6 @@ $('#start h3').click(function() {
 
 //FUNCTION CALLS
 startUp();
-//questionGenerator();
 
 //console.log('keys', Object.keys(questionAnswerBank));
 //console.log('key1', Object.keys(questionAnswerBank)[6]);
